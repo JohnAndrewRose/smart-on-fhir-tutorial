6d2fa1666df0a1f838155d1cbb253d89e5eef54a
John Rose <john.rose@gtri.gatech.edu>
2021-01-12 11:37:46 -0500

    Trying newer version of api

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -8,7 +8,6 @@
     }
 
     function onReady(smart)  {
-      window.smartAPI = smart;
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
@@ -16,7 +15,7 @@
                     type: 'Observation'
                   });
 
-        var appointments = smart.api.fetchAll({type: 'Appointment', patient: patient.id});
+        var appointments = smart.request("Appointment",{patient: patient.id, pageLimit: 2});
 
         $.when(pt, obv, appointments).fail(onError);
 
